0478756ca8a95ea4768fb542e48b0f562de325cc
hussain-althobaiti <1@hussain-althobaiti.sa>
2019-10-27 20:59:47 +0300

    update

---
+++ new/script.js
@@ -0,0 +1,40 @@
+
+
+
+var number;
+number = 56;
+var number2=587;
+
+var sum=number + number2;
+
+// console.log(sum);
+
+// console.log("the sum is :"+ sum);
+
+// str="the sum is :";
+// console.log(str+sum);
+// console.log("the sum is : ",number2+number);
+// console.log("the sum is : "+number2+number);
+// console.log("the sum is : "+(number2+number));
+
+var y =405;
+var x =87;
+var sum = x + y;
+console.log(x+" / "+y+"= "+(x/y));
+
+console.log(x+'*'+y+"=",x*y);
+console.log(x+"+"+y+"=",x+y);
+console.log(x+"-"+y+"=",x-y);
+console.log(`${x} + ${y} = ${sum}`);
+
+document.write(`${x} + ${y} = ${sum}`)
+
+var str ="string";
+var flag ;
+flag=true;
+console.log(flag);
+flag=sum;
+console.log(flag);
+
+console.log("five" * 5); 
+frirstName
